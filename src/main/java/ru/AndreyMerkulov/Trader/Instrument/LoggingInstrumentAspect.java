02c3def40922a843e0e4efbe02df54ee81bdc846
Andrey Merkulov <andrew_aa@mail.ru>
2024-06-02 17:54:59 +0300

    Попытка прикрутить AOP

--- src/main/java/ru/AndreyMerkulov/Trader/Instrument/LoggingInstrumentAspect.java
+++ src/main/java/ru/AndreyMerkulov/Trader/Instrument/LoggingInstrumentAspect.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.stereotype.Component;
 import org.aspectj.lang.annotation.Aspect;
 
+
 @Component
 public class LoggingInstrumentAspect {
 }
