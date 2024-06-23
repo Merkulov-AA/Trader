a341c76744957c3a4b236f665360384ed91e8e5e
Andrey Merkulov <andrew_aa@mail.ru>
2024-06-23 14:47:04 +0300

    1) Добавлен liquiBase 2) убраны ненужные классы

--- src/main/java/ru/andreymerkulov/trader/instrument/serveice/InstrumentService.java
+++ src/main/java/ru/andreymerkulov/trader/instrument/serveice/InstrumentService.java
@@ -1,11 +1,9 @@
 package ru.andreymerkulov.trader.instrument.serveice;
 
-import org.springframework.stereotype.Service;
 import ru.andreymerkulov.trader.instrument.dto.InstrumentDto;
 
 import java.util.List;
 
-@Service
 public interface InstrumentService {
 
     List<InstrumentDto> getAll();
