77726bb0d531c43e4f4812e1b88394b7fb4de8a1
Andrey Merkulov <andrew_aa@mail.ru>
2024-07-01 00:13:25 +0300

    1) Сконфигуриров H2 2) Добавлены юнит тесты для сервиса и контроллера

---
+++ src/test/java/ru/andreymerkulov/trader/instrument/InstrumentControllerTest.java
@@ -0,0 +1,118 @@
+package ru.andreymerkulov.trader.instrument;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import ru.andreymerkulov.trader.instrument.dto.InstrumentDto;
+import ru.andreymerkulov.trader.instrument.serveice.InstrumentService;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.doNothing;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+
+
+@WebMvcTest(InstrumentController.class)
+class InstrumentControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private InstrumentService instrumentService;
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+
+    @Test
+    @WithMockUser
+    void getAll() throws Exception {
+
+        List<InstrumentDto> instruments = Arrays.asList(
+                new InstrumentDto("AAA", 3.0F, 0.001f),
+                new InstrumentDto("BBB", 111.0f, 222.0f)
+        );
+        when(instrumentService.getAll()).thenReturn(instruments);
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/api/v1/instrument"))
+                        .andExpect(status().isOk())
+                        .andExpect(content().json(objectMapper.writeValueAsString(instruments)));
+
+        verify(instrumentService, times(1)).getAll();
+    }
+
+    @Test
+    @WithMockUser
+    void getInstrument() throws Exception {
+
+        InstrumentDto instrumentDto = new InstrumentDto("AAA", 3.0F, 0.001f);
+
+        when(instrumentService.get(instrumentDto.getSymbol())).thenReturn(instrumentDto);
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/api/v1/instrument/get/{symbol}", instrumentDto.getSymbol()))
+                        .andExpect(status().isOk())
+                        .andExpect(content().json(objectMapper.writeValueAsString(instrumentDto)));
+
+        verify(instrumentService, times(1)).get(instrumentDto.getSymbol());
+    }
+
+    @Test
+    @WithMockUser
+    void addInstrument() throws Exception {
+        InstrumentDto instrumentDto = new InstrumentDto("AAA", 3.0F, 0.001f);
+
+        doNothing().when(instrumentService).add(instrumentDto);
+
+        mockMvc.perform(post("/api/v1/instrument/add")
+                        .with(csrf())
+                        .contentType("application/json")
+                        .content(objectMapper.writeValueAsString(instrumentDto)))
+                        .andExpect(status().isOk());
+
+        verify(instrumentService, times(1)).add(instrumentDto);
+    }
+
+    @Test
+    @WithMockUser
+    void delInstrument() throws Exception {
+
+        InstrumentDto instrumentDto = new InstrumentDto("AAA", 3.0F, 0.001f);
+
+        doNothing().when(instrumentService).del(instrumentDto.getSymbol());
+
+        mockMvc.perform(delete("/api/v1/instrument/del/{symbol}", instrumentDto.getSymbol())
+                        .with(csrf()))
+                        .andExpect(status().isOk());
+
+        verify(instrumentService, times(1)).del(instrumentDto.getSymbol());
+    }
+
+    @Test
+    @WithMockUser
+    void updInstrument() throws Exception {
+        InstrumentDto instrumentDto = new InstrumentDto("AAA", 3.0F, 0.001f);
+
+        doNothing().when(instrumentService).upd(instrumentDto);
+
+        mockMvc.perform(post("/api/v1/instrument/upd")
+                        .with(csrf())
+                        .contentType("application/json")
+                        .content(objectMapper.writeValueAsString(instrumentDto)))
+                        .andExpect(status().isOk());
+
+        verify(instrumentService, times(1)).upd(instrumentDto);
+    }
+}
--- src/test/java/ru/andreymerkulov/trader/instrument/service/InstrumentServiceTest.java
+++ src/test/java/ru/andreymerkulov/trader/instrument/service/InstrumentServiceTest.java
@@ -1,108 +1,123 @@
-//package ru.andreymerkulov.trader.instrument.service;
-//
-//
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.extension.ExtendWith;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.beans.factory.annotation.Qualifier;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.boot.test.mock.mockito.MockBean;
-//import org.springframework.test.context.junit.jupiter.SpringExtension;
-//import ru.andreymerkulov.trader.instrument.dto.InstrumentDto;
-//import ru.andreymerkulov.trader.instrument.repository.InstrumentDAO;
-//import ru.andreymerkulov.trader.instrument.serveice.InstrumentService;
-//
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//import static org.assertj.core.api.Assertions.assertThat;
-//import static org.junit.jupiter.api.Assertions.*;
-//import static org.mockito.BDDMockito.given;
-//import static org.mockito.Mockito.*;
-//
-//@SpringBootTest
-//@ExtendWith(SpringExtension.class)
-//class InstrumentServiceTest {
-//
-//    @Autowired
-//    private InstrumentService instrumentService;
-//
-//    @MockBean
-//    private InstrumentDAO instrumentDAO;
-//
-//    @Test
-//    void getAll() {
-//        // Given
-//        InstrumentDto newInstrumentDto1 = new InstrumentDto("BTCUSDT", 3, 0.001f);
-//        InstrumentDto newInstrumentDto2 = new InstrumentDto("ETHUSDT", 4, 0.0001f);
-//        List<InstrumentDto> expectedInstrumentDtos = new ArrayList<>();
-//        expectedInstrumentDtos.add(newInstrumentDto1);
-//        expectedInstrumentDtos.add(newInstrumentDto2);
-//
-//        // Настраиваем заглушку для возвращения ожидаемого списка при вызове метода getAll
-//        when(instrumentDAO.getAll()).thenReturn(expectedInstrumentDtos);
-//
-//        // Then
-//        List<InstrumentDto> instrumentDtos = instrumentService.getAll();
-//        assertThat(instrumentDtos).contains(newInstrumentDto1, newInstrumentDto2);
-//        verify(instrumentDAO, times(1)).getAll();
-//    }
-//
-//    @Test
-//    void testGet(){
-//        // Given
-//        InstrumentDto instrumentDto = new InstrumentDto("BTCUSDT", 3, 0.001f);
-//        given(instrumentDAO.get(instrumentDto.getSymbol())).willReturn(instrumentDto);
-//
-//        // When
-//        InstrumentDto foundInstrumentDto = instrumentService.get(instrumentDto.getSymbol());
-//
-//        // Then
-//        assertNotNull(foundInstrumentDto);
-//        assertEquals(instrumentDto, foundInstrumentDto);
-//    }
-//    @Test
-//    void testAdd(){
-//
-//        // Given
-//        InstrumentDto newInstrumentDto = new InstrumentDto("BTCUSDT", 3, 0.001f);
-//
-//        // Configure mock repository to return expected result
-//        doNothing().when(instrumentDAO).add(newInstrumentDto);
-//
-//        // When
-//        instrumentService.add(newInstrumentDto);
-//
-//        // Then
-//        verify(instrumentDAO, times(1)).add(newInstrumentDto);
-//
-//    }
-//    @Test
-//    void testDel(){
-//        String symbol = "BTCUSDT";
-//
-//        // Configure mock repository to return expected result
-//        doNothing().when(instrumentDAO).del(symbol);
-//
-//        // When
-//        instrumentService.del(symbol);
-//
-//        // Then
-//        verify(instrumentDAO, times(1)).del(symbol);
-//    }
-//    @Test
-//    void testUpd(){
-//        // Given
-//        InstrumentDto updInstrumentDto = new InstrumentDto("BTCUSDT", 3, 0.001f);
-//
-//        // Configure mock repository to return expected result
-//        doNothing().when(instrumentDAO).upd(updInstrumentDto);
-//
-//        // When
-//        instrumentService.upd(updInstrumentDto);
-//
-//        // Then
-//        verify(instrumentDAO, times(1)).upd(updInstrumentDto);
-//    }
-//}
+package ru.andreymerkulov.trader.instrument.service;
+
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import ru.andreymerkulov.trader.instrument.Instrument;
+import ru.andreymerkulov.trader.instrument.dto.InstrumentDto;
+import ru.andreymerkulov.trader.instrument.mapper.InstrumentMapper;
+import ru.andreymerkulov.trader.instrument.repository.InstrumentRepository;
+import ru.andreymerkulov.trader.instrument.serveice.InstrumentService;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.*;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@ExtendWith(SpringExtension.class)
+//@DataJpaTest
+class InstrumentServiceTest {
+
+    @Autowired
+    private InstrumentService instrumentService;
+
+    @MockBean
+    private InstrumentRepository instrumentRepository;
+
+    @Autowired
+    private InstrumentMapper instrumentMapper;
+
+    @Test
+    void getAll() {
+        // Given
+        Instrument newInstrument1 = new Instrument("BTCUSDT", 3, 0.001f);
+        Instrument newInstrument2 = new Instrument("ETHUSDT", 4, 0.0001f);
+        InstrumentDto newInstrumentDto1 = instrumentMapper.instrumentToInstrumentDto(newInstrument1);
+        InstrumentDto newInstrumentDto2 = instrumentMapper.instrumentToInstrumentDto(newInstrument2);
+
+        List<Instrument> expectedInstruments = new ArrayList<>();
+        expectedInstruments.add(newInstrument1);
+        expectedInstruments.add(newInstrument2);
+
+        // Настраиваем заглушку для возвращения ожидаемого списка при вызове метода getAll
+        when(instrumentRepository.findAll()).thenReturn(expectedInstruments);
+
+        // Then
+        List<InstrumentDto> instrumentDtos = instrumentService.getAll();
+        assertThat(instrumentDtos).contains(newInstrumentDto1, newInstrumentDto2);
+        verify(instrumentRepository, times(1)).findAll();
+    }
+
+    @Test
+    void testGet(){
+        // Given
+        Instrument newInstrument = new Instrument("BTCUSDT", 3, 0.001f);
+        InstrumentDto newInstrumentDto = instrumentMapper.instrumentToInstrumentDto(newInstrument);
+        given(instrumentRepository.findById(newInstrument.getSymbol())).willReturn(Optional.of(newInstrument));
+
+        // When
+        InstrumentDto foundInstrumentDto = instrumentService.get(newInstrument.getSymbol());
+
+        // Then
+        assertNotNull(foundInstrumentDto);
+        assertEquals(newInstrumentDto, foundInstrumentDto);
+    }
+    @Test
+    void testAdd(){
+
+        // Given
+        Instrument newInstrument = new Instrument("BTCUSDT", 3, 0.001f);
+        InstrumentDto newInstrumentDto = instrumentMapper.instrumentToInstrumentDto(newInstrument);
+
+        // Configure mock repository to return expected result
+        when(instrumentRepository.save(newInstrument)).thenReturn(any());
+
+        // When
+        instrumentService.add(newInstrumentDto);
+
+        // Then
+        verify(instrumentRepository, times(1)).save(newInstrument);
+
+    }
+    @Test
+    void testDel(){
+        Instrument instrument = new Instrument("BTCUSDT", 3, 0.001f);
+
+        // Configure mock repository to return expected result
+        doNothing().when(instrumentRepository).deleteById(instrument.getSymbol());
+
+        // When
+        instrumentService.del(instrument.getSymbol());
+
+        // Then
+        verify(instrumentRepository, times(1)).deleteById(instrument.getSymbol());
+    }
+    @Test
+    void testUpd(){
+        // Given
+        Instrument instrument = new Instrument("BTCUSDT", 3, 0.001f);
+        InstrumentDto instrumentDto = new InstrumentDto("BTCUSDT", 3, 0.001f);
+
+        // Configure mock repository to return expected result
+        when(instrumentRepository.save(instrument)).thenReturn(any());
+
+
+        // When
+        instrumentService.upd(instrumentDto);
+
+        // Then
+        verify(instrumentRepository, times(1)).save(instrument);
+    }
+}
