1433669d5341ad1d480b86d3095fcd13b4868e21
Andrey Merkulov <andrew_aa@mail.ru>
2024-07-06 01:28:40 +0300

    Добавил Пользователя и настроил безопасность

--- src/main/java/ru/andreymerkulov/trader/glob/config/SecurityConfig.java
+++ src/main/java/ru/andreymerkulov/trader/glob/config/SecurityConfig.java
@@ -3,17 +3,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 
 
@@ -26,10 +25,10 @@ public class SecurityConfig {
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry ->
                         authorizationManagerRequestMatcherRegistry
-                                .requestMatchers(HttpMethod.DELETE, "/api/v1/instrument/**").hasAnyRole("ADMIN")
-                                .requestMatchers(HttpMethod.POST, "/api/v1/instrument/**").hasAnyRole("ADMIN")
-                                .requestMatchers(HttpMethod.GET, "/api/v1/instrument").hasAnyRole("USER", "ADMIN")
-                                .requestMatchers(HttpMethod.GET, "/api/v1/instrument/**").hasAnyRole("USER", "ADMIN")
+                                .requestMatchers(HttpMethod.DELETE, "/api/v1/instrument/**").hasAnyAuthority("ADMIN")
+                                .requestMatchers(HttpMethod.POST, "/api/v1/instrument/**").hasAnyAuthority("ADMIN")
+                                .requestMatchers(HttpMethod.GET, "/api/v1/instrument").hasAnyAuthority("USER", "ADMIN")
+                                .requestMatchers(HttpMethod.GET, "/api/v1/instrument/**").hasAnyAuthority("USER", "ADMIN")
                                 .anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults())
                 .sessionManagement(httpSecuritySessionManagementConfigurer -> httpSecuritySessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
@@ -38,27 +37,42 @@ public class SecurityConfig {
     }
 
     @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user = User.builder()
-                .username("user")
-                .password(passwordEncoder().encode("pwd"))
-                .roles("USER")
+    public AuthenticationManager authenticationManager(
+            HttpSecurity http,
+            PasswordEncoder passwordEncoder,
+            UserDetailsService userDetailService
+    )
+            throws Exception {
+        return http.getSharedObject(AuthenticationManagerBuilder.class)
+                .userDetailsService(userDetailService)
+                .passwordEncoder(passwordEncoder)
+                .and()
                 .build();
-        UserDetails admin = User.builder()
-                .username("admin")
-                .password(passwordEncoder().encode("pwd"))
-                .roles("ADMIN")
-                .build();
-        UserDetails test = User.builder()
-                .username("test")
-                .password(passwordEncoder().encode("pwd"))
-                .roles("TEST")
-                .build();
-        return new InMemoryUserDetailsManager(user, admin, test);
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        return NoOpPasswordEncoder.getInstance();
     }
+
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//        UserDetails user = User.builder()
+//                .username("user")
+//                .password(passwordEncoder().encode("pwd"))
+//                .roles("USER")
+//                .build();
+//        UserDetails admin = User.builder()
+//                .username("admin")
+//                .password(passwordEncoder().encode("pwd"))
+//                .roles("ADMIN")
+//                .build();
+//        UserDetails test = User.builder()
+//                .username("test")
+//                .password(passwordEncoder().encode("pwd"))
+//                .roles("TEST")
+//                .build();
+//        return new InMemoryUserDetailsManager(user, admin, test);
+//    }
+
 }
